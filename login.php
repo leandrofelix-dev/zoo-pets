23a7f14bfacdf3cd7d47360b4b18b5b4b778ec6c
lehinfo.felix <lehinfo.felix@gmail.com>
2020-04-02 19:28:17 -0300

    correção de bugs

--- login.php
+++ login.php
@@ -3,17 +3,3 @@
     require('conect.php');
     require('./cabecalho.php');
 ?>
-
-    <form id="login-form" class="flex">
-    <a href="" onclick="fechar_login()"><img src="icons/close.svg" alt=""></a>
-    <div class="form-group">
-      <label for="exampleInputEmail1">ENDEREÇO DE E-MAIL</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email">
-      <small id="emailHelp" class="form-text text-muted">Nunca vamos compartilhar seu email, com ninguém.</small>
-    </div>
-    <div class="form-group">
-      <label for="exampleInputPassword1">SENHA</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
-    </div>
-    <button type="submit" class="btn btn-primary">Enviar</button>
-  </form>
